6c340dc18a916c4ae0fe4265bb2aa2c4f9e6ca47
Kenny Elshoff <github@kennyelshoff.com>
2023-05-04 20:38:14 -0400

    chore: configure eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+/* eslint-env node */
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+  ],
+  plugins: ["@typescript-eslint"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: ["./tsconfig.eslint.json", "./packages/functions/tsconfig.json"],
+    tsconfigRootDir: __dirname,
+  },
+  root: true,
+};
---
+++ stacks/StorageStack.ts
@@ -0,0 +1,16 @@
+import { Table, StackContext } from "sst/constructs";
+
+export function StorageStack({ stack, app }: StackContext) {
+  // Create the DynamoDB table
+  const table = new Table(stack, "Notes", {
+    fields: {
+      userId: "string",
+      noteId: "string",
+    },
+    primaryIndex: { partitionKey: "userId", sortKey: "noteId" },
+  });
+
+  return {
+    table,
+  };
+}
