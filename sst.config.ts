f5f697726f54a5015375b3a643a4e5b337005134
Kenny Elshoff <github@kennyelshoff.com>
2024-02-15 23:13:37 -0500

    feat: add auth stack

--- sst.config.ts
+++ sst.config.ts
@@ -1,6 +1,7 @@
 import { SSTConfig } from "sst";
 import { StorageStack } from "./stacks/StorageStack";
 import { ApiStack } from "./stacks/ApiStack";
+import { AuthStack } from "./stacks/AuthStack";
 
 export default {
   config(_input) {
@@ -10,6 +11,6 @@ export default {
     };
   },
   stacks(app) {
-    app.stack(StorageStack).stack(ApiStack);
+    app.stack(StorageStack).stack(ApiStack).stack(AuthStack);
   },
 } satisfies SSTConfig;
