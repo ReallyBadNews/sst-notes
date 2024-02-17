449d5a25751ec3793819c4187eb4828d8bd898ae
Kenny Elshoff <github@kennyelshoff.com>
2024-02-17 14:31:12 -0500

    feat: init remix site

---
+++ stacks/FrontendStack.ts
@@ -0,0 +1,28 @@
+import { StackContext, RemixSite, use } from "sst/constructs";
+import { ApiStack } from "./ApiStack";
+import { AuthStack } from "./AuthStack";
+import { StorageStack } from "./StorageStack";
+
+export function FrontendStack({ stack, app }: StackContext) {
+  const { api } = use(ApiStack);
+  const { auth } = use(AuthStack);
+  const { bucket } = use(StorageStack);
+
+  const site = new RemixSite(stack, "RemixSite", {
+    path: "packages/frontend",
+    buildCommand: "pnpm run build",
+    environment: {
+      VITE_API_URL: api.url,
+      VITE_REGION: app.region,
+      VITE_BUCKET: bucket.bucketName,
+      VITE_USER_POOL_ID: auth.userPoolId,
+      VITE_USER_POOL_CLIENT_ID: auth.userPoolClientId,
+      VITE_IDENTITY_POOL_ID: auth.cognitoIdentityPoolId || "",
+    },
+  });
+
+  // Show the url in the output
+  stack.addOutputs({
+    SiteUrl: site.url,
+  });
+}
