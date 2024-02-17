1244436401c3cfa390e0c38e6094ddc5868d174b
Kenny Elshoff <github@kennyelshoff.com>
2024-02-17 09:50:31 -0500

    chore: add stripe api key

--- stacks/ApiStack.ts
+++ stacks/ApiStack.ts
@@ -1,15 +1,16 @@
-import { Api, StackContext, use } from "sst/constructs";
+import { Api, Config, StackContext, use } from "sst/constructs";
 import { StorageStack } from "./StorageStack";
 
 export function ApiStack({ stack }: StackContext) {
   const { table } = use(StorageStack);
+  const STRIPE_SECRET_KEY = new Config.Secret(stack, "STRIPE_SECRET_KEY");
 
   // Create the API
   const api = new Api(stack, "Api", {
     defaults: {
       authorizer: "iam",
       function: {
-        bind: [table],
+        bind: [table, STRIPE_SECRET_KEY],
       },
     },
     routes: {
